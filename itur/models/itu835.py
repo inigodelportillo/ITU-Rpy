39e1e1c711b52b17263eff76e5666ab2f87e22b7
Inigo del Portillo <portillo@mit.edu>
2018-05-22 19:49:40 -0400

    Bug fix: Use list as input for 'standard_pressure' (ITU-R P.676)

--- itur/models/itu835.py
+++ itur/models/itu835.py
@@ -445,9 +445,8 @@ class _ITU835_5():
     def low_latitude_pressure(self, h):
         """Section 2 of Recommendation ITU-R P.835
         """
-
-        P10 = self.standard_pressure(10)
-        P72 = self.standard_pressure(72)
+        P10 = self.standard_pressure([10])[0]
+        P72 = self.standard_pressure([72])[0]
         return np.where(np.logical_and((0 <= h), (h <= 10)),
                         1012.0306 - 109.0338 * h + 3.6316 * h ** 2,
                np.where(np.logical_and( (10 < h), (h <= 72)),
@@ -480,8 +479,8 @@ class _ITU835_5():
     def mid_latitude_pressure_summer(self, h):
         """Section 4.1 of Recommendation ITU-R P.835
         """
-        P10 = self.standard_pressure(10)
-        P72 = self.standard_pressure(72)
+        P10 = self.standard_pressure([10])[0]
+        P72 = self.standard_pressure([72])[0]
         return np.where(
             np.logical_and((0 <= h), (h <= 10)),
             1008.0278 - 113.2494 * h + 3.9408 * h ** 2, np.where(
@@ -515,8 +514,8 @@ class _ITU835_5():
     def mid_latitude_pressure_winter(self, h):
         """Section 4.2 of Recommendation ITU-R P.835
         """
-        P10 = self.standard_pressure(10)
-        P72 = self.standard_pressure(72)
+        P10 = self.standard_pressure([10])[0]
+        P72 = self.standard_pressure([72])[0]
         return np.where(np.logical_and((0 <= h), (h <= 10)),
                         1010.8828 - 122.2411 * h + 4.554 * h ** 2,
                np.where(np.logical_and((10 < h), (h <= 72)),
@@ -549,8 +548,8 @@ class _ITU835_5():
     def high_latitude_pressure_summer(self, h):
         """Section 3.1 of Recommendation ITU-R P.835
         """
-        P10 = self.standard_pressure(10)
-        P72 = self.standard_pressure(72)
+        P10 = self.standard_pressure([10])[0]
+        P72 = self.standard_pressure([72])[0]
         return np.where(np.logical_and((0 <= h), (h <= 10)),
                         1012.8186 - 111.5569 * h + 3.8646 * h ** 2,
                np.where(np.logical_and((10 < h), (h <= 72)),
@@ -582,8 +581,8 @@ class _ITU835_5():
     def high_latitude_pressure_winter(self, h):
         """Section 3.2 of Recommendation ITU-R P.835
         """
-        P10 = self.standard_pressure(10)
-        P72 = self.standard_pressure(72)
+        P10 = self.standard_pressure([10])[0]
+        P72 = self.standard_pressure([72])[0]
         return np.where(np.logical_and((0 <= h), (h <= 10)),
                         1018.8627 - 124.2954 * h + 4.8307 * h ** 2,
                np.where(np.logical_and((10 < h), (h <= 72)),
