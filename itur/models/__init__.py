88d7cec720e1f19d4aed6ae5341a144259f0bb69
Inigo del Portillo <inigo.del.portillo@gmail.com>
2021-04-09 13:13:10 -0400

    Add base ITU-R P.1623
    Based on code provided by Luis Emiliani

--- itur/models/__init__.py
+++ itur/models/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['itu453', 'itu530', 'itu618', 'itu676', 'itu835', 'itu836',
            'itu837', 'itu838', 'itu839', 'itu840', 'itu1144', 'itu1510',
-           'itu1511', 'itu1853']
+           'itu1511', 'itu1623', 'itu1853']
 
 import itur.models.itu453
 import itur.models.itu530
@@ -14,4 +14,5 @@ import itur.models.itu840
 import itur.models.itu1144
 import itur.models.itu1510
 import itur.models.itu1511
+import itur.models.itu1623
 import itur.models.itu1853
