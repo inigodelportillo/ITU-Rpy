ede67eb8c3b19837f8229addf9007ca02f33f1b9
Inigo del Portillo <inigo.del.portillo@ses.com>
2021-08-17 19:58:12 +0000

    Remove pandas warning

--- test/ITU_validation_report_test.py
+++ test/ITU_validation_report_test.py
@@ -11,7 +11,11 @@ import itur.models as models
 from itur import atmospheric_attenuation_slant_path
 
 
-pd.set_option('display.max_colwidth', None)
+if sys.version[:3] <= '3.5':
+    pd.set_option('display.max_colwidth', -1)
+else:
+    pd.set_option('display.max_colwidth', None)
+
 basepath = path.dirname(path.realpath(__file__))
 test_data = path.join(basepath, 'test_data')
 html_path = path.join(basepath, '../docs/validation')
