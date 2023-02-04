eaba6283c0ed5fd9d57dc52d29db24c4d6f276b6
Inigo del Portillo <inigo.del.portillo@gmail.com>
2023-02-04 17:37:43 -0500

    Update supported python versions.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open as open_codecs
 from os import path
@@ -10,7 +11,7 @@ import itur
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open_codecs(path.join(here, 'README.rst')) as f:
+with open_codecs(path.join(here, "README.rst")) as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -28,8 +29,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='itur',
-
+    name="itur",
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
@@ -37,12 +37,10 @@ setup(
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version=itur.__version__,
-
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A python implementation of the ITU-R P. Recommendations',
-
+    description="A python implementation of the ITU-R P. Recommendations",
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
@@ -52,23 +50,18 @@ setup(
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
-
     # This should be a valid link to your project's main homepage.
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/inigodelportillo/ITU-Rpy',
-
+    url="https://github.com/inigodelportillo/ITU-Rpy",
     # This should be your name or the name of the organization which owns the
     # project.
-    author='Inigo del Portillo',
-
+    author="Inigo del Portillo",
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='inigo.del.portillo@gmail.com',
-
-    license='MIT',
-
+    author_email="inigo.del.portillo@gmail.com",
+    license="MIT",
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see
@@ -78,34 +71,26 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Telecommunications Industry',
-        'Topic :: Scientific/Engineering :: Physics',
-
+        "Intended Audience :: Telecommunications Industry",
+        "Topic :: Scientific/Engineering :: Physics",
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
-
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?
     #
     # Note that this is a string of words separated by whitespace, not a list.
-    keywords='atmopheric-propagation attenuation communications',
-
+    keywords="atmopheric-propagation attenuation communications",
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -115,37 +100,36 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['astropy', 'scipy', 'numpy', 'pyproj'],  # Optional
-
+    install_requires=["astropy", "scipy", "numpy", "pyproj"],  # Optional
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     #
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
     package_data={  # Optional
-        'itur': ['LICENSE.txt',
-                 'README.md',
-                 'data/453/*.npz',
-                 'data/530/*.npz',
-                 'data/676/*.txt',
-                 'data/836/*.npz',
-                 'data/837/*.npz',
-                 'data/839/*.npz',
-                 'data/840/*.npz',
-                 'data/1510/*.npz',
-                 'data/1511/*.npz']
+        "itur": [
+            "LICENSE.txt",
+            "README.md",
+            "data/453/*.npz",
+            "data/530/*.npz",
+            "data/676/*.txt",
+            "data/836/*.npz",
+            "data/837/*.npz",
+            "data/839/*.npz",
+            "data/840/*.npz",
+            "data/1510/*.npz",
+            "data/1511/*.npz",
+        ]
     },
-
-	project_urls={  # Optional
-        'Bug Reports': 'https://github.com/inigodelportillo/ITU-Rpy/issues',
-        'Source': 'https://github.com/inigodelportillo/ITU-Rpy/',
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/inigodelportillo/ITU-Rpy/issues",
+        "Source": "https://github.com/inigodelportillo/ITU-Rpy/",
     },
 )
