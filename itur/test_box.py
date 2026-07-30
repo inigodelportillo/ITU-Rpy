dd0f96325baf4b095f925c41b5b2066a0fb8d73a
Calvin Chang <E36331@aero.org>
2026-07-30 10:03:56 -0700

    removing sandbox test file that I accidentally pushed

--- itur/test_box.py
+++
@@ -1,9 +0,0 @@
-from models.itu2145 import (
-    barometric_surface_pressure,
-    surface_temperature,
-    surface_water_vapour_density,
-    total_water_vapour_content
-)
-import models.itu836 as old
-surface_water_vapour_density(0,179.75,0.1,0)
-pass
